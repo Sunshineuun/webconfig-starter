a112e92b7ab3cc0dc4af7a61bb9f6b1bee411392
邱胜明 <qiushengming@simmed.cn>
2023-08-03 09:30:01 +0800

    feat: 当返回string时，会带双引号问题解决，主要是在转换的时候string用了fastjson，所以带了双引号，现在用StringHttpMessageConverter去处理

--- src/main/java/icu/uun/starter/WebConfig.java
+++ src/main/java/icu/uun/starter/WebConfig.java
@@ -1,11 +1,24 @@
 package icu.uun.starter;
 
+import com.alibaba.fastjson.PropertyNamingStrategy;
+import com.alibaba.fastjson.serializer.SerializeConfig;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author qiushengming
@@ -21,6 +34,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+
         if (this.buildInterceptorRegistry != null) {
             this.buildInterceptorRegistry.register(registry);
         }
@@ -36,5 +50,47 @@ public class WebConfig implements WebMvcConfigurer {
         if (this.buildResourceHandlerRegistry != null) {
             this.buildResourceHandlerRegistry.register(registry);
         }
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(stringHttpMessageConverterApplicationJson());
+        converters.add(fastJsonHttpMessageConverter());
+    }
+
+    @Bean
+    public HttpMessageConverter<String> stringHttpMessageConverterApplicationJson() {
+        List<MediaType> list = new ArrayList<>();
+        list.add(MediaType.APPLICATION_JSON);
+
+        StringHttpMessageConverter stringHttpMessageConverter = new StringHttpMessageConverter();
+        stringHttpMessageConverter.setSupportedMediaTypes(list);
+        return stringHttpMessageConverter;
+    }
+
+    @Bean
+    public HttpMessageConverter<Object> fastJsonHttpMessageConverter() {
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+        FastJsonConfig config = new FastJsonConfig();
+        config.setDateFormat("yyy-MM-dd HH:mm:ss");
+        config.setCharset(StandardCharsets.UTF_8);
+        config.setSerializerFeatures(
+//                SerializerFeature.WriteClassName,
+                SerializerFeature.WriteMapNullValue,
+                SerializerFeature.PrettyFormat,
+                SerializerFeature.WriteNullListAsEmpty,
+                SerializerFeature.WriteNullStringAsEmpty
+        );
+        SerializeConfig serializeConfig = new SerializeConfig();
+        serializeConfig.setPropertyNamingStrategy(PropertyNamingStrategy.SnakeCase);
+        config.setSerializeConfig(serializeConfig);
+
+        converter.setFastJsonConfig(config);
+        converter.setDefaultCharset(StandardCharsets.UTF_8);
+        List<MediaType> mediaTypeList = new ArrayList<>();
+        // 解决中文乱码问题，相当于在Controller上的@RequestMapping中加了个属性produces = "application/json"
+        mediaTypeList.add(MediaType.APPLICATION_JSON);
+        converter.setSupportedMediaTypes(mediaTypeList);
+        return converter;
     }
 }
