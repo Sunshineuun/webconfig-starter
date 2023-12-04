0a4e017acffbfb6cb258773d6fc921037292b60b
邱胜明 <qiushengming@simmed.cn>
2023-12-04 13:46:08 +0800

    feat: druid sql 监控数据持久化，增加id 使用雪花算法

--- src/main/java/icu/uun/starter/druid/DahaStatLogger.java
+++ src/main/java/icu/uun/starter/druid/DahaStatLogger.java
@@ -1,6 +1,8 @@
 package icu.uun.starter.druid;
 
 import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.lang.Snowflake;
+import cn.hutool.core.util.IdUtil;
 import com.alibaba.druid.pool.DruidDataSourceStatLogger;
 import com.alibaba.druid.pool.DruidDataSourceStatLoggerAdapter;
 import com.alibaba.druid.pool.DruidDataSourceStatValue;
@@ -11,11 +13,16 @@ import icu.uun.starter.cat.InetUtils;
 import icu.uun.starter.util.CommonUtil;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.SystemUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
 
 /**
  * @author qiushengming
@@ -30,6 +37,8 @@ public class DahaStatLogger extends DruidDataSourceStatLoggerAdapter implements 
     @Value("${spring.application.name}")
     private String applicationName;
     public static String TOPIC;
+    private final Snowflake secondSnow = IdUtil.getSnowflake(getWorkId(), getDataCenterId());
+
 
     @Value("${uun.monitor.druidSql.topic:MONITOR_DRUID_SQL}")
     public void setTopic(String topic) {
@@ -50,9 +59,8 @@ public class DahaStatLogger extends DruidDataSourceStatLoggerAdapter implements 
                 try {
                     DruidSqlMonitor druidSqlMonitor = JSONObject
                             .parseObject(JSONObject.toJSONString(sqlStat.getData()), DruidSqlMonitor.class);
-                    String id = String.format("%s_%s_%s", applicationName, druidSqlMonitor.getHash().toString(),
-                            druidSqlMonitor.getId());
-                    druidSqlMonitor.setId(id);
+                    druidSqlMonitor.setDataSource(applicationName);
+                    druidSqlMonitor.setId(secondSnow.nextIdStr());
                     druidSqlMonitor.setIp(this.ip);
                     druidSqlMonitor.setUpdateTime(DateUtil.now());
                     kafkaTemplate.send(TOPIC, JSONObject.toJSONString(druidSqlMonitor));
@@ -60,6 +68,47 @@ public class DahaStatLogger extends DruidDataSourceStatLoggerAdapter implements 
                     log.error("druid sql monitor: {}", CommonUtil.toStackTraceStr(e));
                 }
             }
+        }
+    }
+
+    /**
+     * workId使用IP生成
+     * @return workId
+     */
+    private static Long getWorkId() {
+        try {
+            String hostAddress = Inet4Address.getLocalHost().getHostAddress();
+            int[] ints = StringUtils.toCodePoints(hostAddress);
+            int sums = 0;
+            for (int b : ints) {
+                sums = sums + b;
+            }
+            return (long) (sums % 32);
+        }
+        catch (UnknownHostException e) {
+            // 失败就随机
+            return RandomUtils.nextLong(0, 31);
+        }
+    }
+
+
+    /**
+     * dataCenterId使用hostName生成
+     * @return dataCenterId
+     */
+    private static Long getDataCenterId() {
+        try {
+            String hostName = SystemUtils.getHostName();
+            int[] ints = StringUtils.toCodePoints(hostName);
+            int sums = 0;
+            for (int i: ints) {
+                sums = sums + i;
+            }
+            return (long) (sums % 32);
+        }
+        catch (Exception e) {
+            // 失败就随机
+            return RandomUtils.nextLong(0, 31);
         }
     }
 }
